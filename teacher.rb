6d30f1f864c7ca62cc452b5bdbbab1d9f055516b
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-09-26 23:46:12 +0100

    add specialization and classroom

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'Person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name: 'unknown')
+  def initialize(age, specialization = 'Java', name: 'unknown')
     super(age, name)
     @specialization = specialization
   end
