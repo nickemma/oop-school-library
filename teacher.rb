2cf2b3b230548b0597220e20170ca6d5a4d95508
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-09-26 23:15:29 +0100

    add the instance of teacher

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative 'Person'
+
+class Teacher < Person
+ attr_accessor :specialization
+
+ def initialize(age, specialization, name: 'unknown')
+  super(age, name)
+  @specialization = specialization
+ end
+
+ def can_use_services?
+  true
+ end
+end
