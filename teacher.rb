cb30c940d77ebbc9880618020a5f851174e4a0d1
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-09-26 23:25:35 +0100

    fixed rubocop error

--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,14 @@
 require_relative 'Person'
 
 class Teacher < Person
- attr_accessor :specialization
+  attr_accessor :specialization
 
- def initialize(age, specialization, name: 'unknown')
-  super(age, name)
-  @specialization = specialization
- end
+  def initialize(age, specialization, name: 'unknown')
+    super(age, name)
+    @specialization = specialization
+  end
 
- def can_use_services?
-  true
- end
+  def can_use_services?
+    true
+  end
 end
