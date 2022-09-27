599a36548c0764337c3da9f4a09e350e0261d95f
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-09-27 01:30:30 +0100

    fixed error

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'Person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'unknown', parent_permission = true)
+  def initialize(age, specialization, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
