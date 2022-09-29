2bc1569db5bc3c41710ec7574f62b87812cc6691
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-09-29 08:07:16 +0100

    add the initializer

--- app.rb
+++ app.rb
@@ -4,3 +4,15 @@ require_relative 'rental'
 require_relative 'student'
 require_relative 'teacher'
 require_relative 'classroom'
+
+class App
+ attr_accessor :books_list, :people
+
+ def initialize
+   @books_list = []
+   @people = []
+   @rentals = []
+   @students = []
+   @teachers = []
+ end
+end
