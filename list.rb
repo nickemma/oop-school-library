e2168123d0edf8c88b0deedce475b5233a60697c
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-10-04 22:02:34 +0100

    fixed rubocop errors

--- list.rb
+++ list.rb
@@ -103,7 +103,7 @@ class List
     puts 'Enter ID of person: '
     id = gets.chomp.to_i
     puts 'Rentals:'
-    @rentals.each do |rental| # shadowing outer `foo`
+    @rentals.each do |rental|
       puts "Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
     end
   end
