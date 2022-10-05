1963feda4031b2841f8321c2216333f1f150aa10
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-10-05 12:04:36 +0100

    add the test for student

---
+++ spec/student_spec.rb
@@ -0,0 +1,17 @@
+require_relative('spec_helper')
+
+describe Student do
+  let(:student) { Student.new(20, 'John', 'Maths') }
+
+  describe '#new' do
+    it 'Takes three parameters and returns a Student object' do
+      expect(student).to be_an_instance_of(Student)
+    end
+  end
+
+  describe '#play_hooky' do
+    it 'Returns the correct string' do
+      expect(student.play_hooky).to eq("¯\\(ツ)/¯")
+    end
+  end
+end
